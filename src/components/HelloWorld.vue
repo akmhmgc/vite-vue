a419caf0e2f87a27ba25bafc2e781ffbb1ba2e99
akmhmgc <akimuhamaguchi@icloud.com>
2022-03-06 22:15:42 +0900

    Remove unused code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,50 +1,22 @@
 <script setup lang="ts">
-import { ref, onMounted, onUpdated } from "vue";
+import { ref, onUpdated } from "vue";
 import axios from "axios";
 
-defineProps<{ msg: string }>();
-
-const count = ref(0);
-const msg = ref("");
 const address = ref("");
 const postcode = ref("");
-
-const add = (): void => {
-  count.value += 1;
-};
 
 onUpdated(async () => {
   const zipcode = postcode.value;
   if (zipcode.length !== 7) return;
 
-  await axios
-    .get(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipcode}`)
-    .then((response) => {
-      address.value = response.data.results[0].address3;
-    });
+  const response = await axios.get(
+    `https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipcode}`
+  );
+  address.value = response.data.results[0].address3;
 });
 </script>
 
 <template>
-  <button type="button" @click="add" id="count">count is: {{ count }}</button>
   <input v-model="postcode" id="textBox" />
   <p>住所: {{ address }}</p>
 </template>
-g
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
